17273c530e1602d006dc012f015d316c13d6015a
KatyBrown <thekatybrown@gmail.com>
2021-12-07 14:52:03 +0000

    wrote part of style guide section

--- code_snippets/wrc_python_style_guides_example1_bad.py
+++ code_snippets/wrc_python_style_guides_example1_bad.py
@@ -4,12 +4,11 @@ def f(s1,s2):
  s3=str(s1)
  s4=str(s2)
  s11=s2/s1
- s11=str(s11)
- s11=s11[0:3]
+ s12=round(s11,2)
+ s11=str(s12)
  print('number of entries ='+q+s3+x+'number of winners='+q+s4+x+'% success ='+q+s11)
 
 r=12
 r1=5
 
 f(r, r1)
-
